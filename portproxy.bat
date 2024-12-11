ddb0cd25c5d238ca35c4708cb3bd9c7c1f42540d
fathulkirom22 <kirom222@gmail.com>
2024-12-12 01:58:37 +0700

    fix: port forwarding

--- portproxy.bat
+++ portproxy.bat
@@ -1,4 +1,18 @@
 @echo off
-netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=8081 connectaddress=172.25.117.33 connectport=8081
-netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=8082 connectaddress=172.25.117.33 connectport=8082
-netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=8096 connectaddress=172.25.117.33 connectport=8096
+
+$wsl_ip = wsl -- ip -o -4 -j addr s eth0 | ConvertFrom-Json | %{ $_.addr_info.local } | ?{ $_ }
+
+@REM nextclud
+netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=8081 connectaddress=$wsl_ip connectport=8081
+
+@REM qbittrren
+netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=8082 connectaddress=$wsl_ip connectport=8082
+
+@REM jellyfin
+netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=8096 connectaddress=$wsl_ip connectport=8096
+
+@REM ssh
+netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=22 connectaddress=$wsl_ip connectport=22
+
+@REM portainer
+netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=9000 connectaddress=$wsl_ip connectport=9000
